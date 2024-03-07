7d0db05b90d392b976e09e317bf3d6372996340f
Santiago <santi@linkedai.co>
2024-03-07 12:29:51 -0500

    Navbar Icon and animations, Theme and Font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    colors: {
+      white: "#fff",
+      black: "#000",
+      blue: "#1649FF",
+      ligthBlue: "#21A2FF"
+    },
+  },
+  plugins: [],
+};
