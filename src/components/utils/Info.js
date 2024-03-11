6f2aff3959b9c607a9a616eb2035d3988ad6c98f
Santiago <santi@linkedai.co>
2024-03-11 17:07:45 -0500

    Portfolio V1

--- src/components/utils/Info.js
+++ src/components/utils/Info.js
@@ -1,139 +1,156 @@
 import ReactJS from "../../icons/stack/React.astro";
 import MaterialUI from "../../icons/stack/MaterialUI.astro";
+import Redux from "../../icons/stack/Redux.astro";
+import Sass from "../../icons/stack/Sass.astro";
+import AstroFramework from "../../icons/stack/AstroFramework.astro";
+import GitHub from "../../icons/GitHub.astro";
+import GitLab from "../../icons/stack/GitLab.astro";
+import JavaScript from "../../icons/stack/JavaScript.astro";
+import TypeScript from "../../icons/stack/TypeScript.astro";
+import Jest from "../../icons/stack/Jest.astro";
+import Vitest from "../../icons/stack/Vitest.astro";
+import CSS from "../../icons/stack/CSS.astro";
+import HTML from "../../icons/stack/HTML.astro";
+import TailwindCSS from "../../icons/stack/TailwindCSS.astro";
+import ThreeJS from "../../icons/stack/ThreeJS.astro";
+import GraphQL from "../../icons/stack/GraphQL.astro";
 
 const TAGS = {
   REACT: {
     name: "React.js",
-    class: "bg-blue-500 text-white",
+    class: "text-white",
     icon: ReactJS,
   },
   MATERIALUI: {
     name: "Material UI",
-    class: "bg-blue-700 text-white",
+    class: "text-white",
     icon: MaterialUI,
   },
   GRAPHQL: {
     name: "GraphQL",
-    class: "bg-pink-500 text-white",
-    icon: "GraphQL",
+    class: "text-white",
+    icon: GraphQL,
   },
   REDUX: {
     name: "Redux",
-    class: "bg-purple-700 text-white",
-    icon: "Redux",
+    class: "text-white",
+    icon: Redux,
   },
   SASS: {
-    name: "Sass/CSS",
-    class: "bg-pink-300 text-white",
-    icon: "Sass",
+    name: "Sass",
+    class: "text-white",
+    icon: Sass,
   },
   HTML: {
     name: "HTML",
-    class: "bg-orange-500 text-white",
-    icon: "HTML",
+    class: "text-white",
+    icon: HTML,
   },
   CSS: {
     name: "CSS",
-    class: "bg-blue-300 text-white",
-    icon: "CSS",
+    class: "text-white",
+    icon: CSS,
   },
   TAILWIND: {
     name: "Tailwind CSS",
-    class: "bg-cyan-500 text-white",
-    icon: "Tailwind",
+    class: "text-white",
+    icon: TailwindCSS,
   },
   THREE_JS: {
     name: "Three.js",
-    class: "bg-black text-white",
-    icon: "Three.js",
+    class: "text-white",
+    icon: ThreeJS,
   },
   ASTRO: {
     name: "Astro",
-    class: "bg-indigo-500 text-white",
-    icon: "Astro",
+    class: "text-white",
+    icon: AstroFramework,
   },
   AWS: {
     name: "AWS",
-    class: "bg-orange-400 text-white",
+    class: "text-white",
     icon: "Aws",
   },
   GITHUB: {
     name: "GitHub",
-    class: "bg-gray-800 text-white",
-    icon: "GitHub",
+    class: "text-white",
+    icon: GitHub,
   },
   GITLAB: {
     name: "GitLab",
-    class: "bg-orange-600 text-white",
-    icon: "GitLab",
+    class: "text-white",
+    icon: GitLab,
   },
   BITBUCKET: {
     name: "Bitbucket",
-    class: "bg-blue-800 text-white",
+    class: "text-white",
     icon: "Bitbucket",
   },
   JAVASCRIPT: {
     name: "JavaScript",
-    class: "bg-yellow-400 text-black",
-    icon: "Javascript",
+    class: "text-white",
+    icon: JavaScript,
   },
   TYPESCRIPT: {
     name: "TypeScript",
-    class: "bg-blue-800 text-white",
-    icon: "Typescript",
+    class: "text-white",
+    icon: TypeScript,
   },
   JEST: {
     name: "Jest",
-    class: "bg-red-500 text-white",
-    icon: "Jest",
+    class: "text-white",
+    icon: Jest,
   },
   TESTING_LIBRARY: {
     name: "Testing Library",
-    class: "bg-pink-600 text-white",
+    class: "text-white",
     icon: "Testing-Library",
   },
   VITEST: {
     name: "Vitest",
-    class: "bg-green-500 text-white",
-    icon: "Vitest",
+    class: "text-white",
+    icon: Vitest,
   },
   NEXT_JS: {
     name: "Next.js",
-    class: "bg-gray-900 text-white",
+    class: "text-white",
     icon: "Next.js",
   },
 };
 
 const PROJECTS = [
   {
-    title: "Usability Improvement in LinkedAI",
-    description:
-      "Optimized LinkedAI platform usability with AI for auto image tagging and high-performance viewer development for enhanced user interaction.",
-    tecnologia: "React",
-    link: "",
-    github: "",
-    imagen: "/images/projects/linkedai.webp",
-    tags: [TAGS.REACT, TAGS.MATERIALUI, TAGS.GRAPHQL, TAGS.REDUX],
+    title: "LinkedAI platform",
+    description: "The world's fastest computer vision data training platform.",
+    link: "https://platform.linkedai.co/login",
+    repo: "",
+    image: "/images/projects/linkedai.webp",
+    tags: [
+      TAGS.REACT,
+      TAGS.JAVASCRIPT,
+      TAGS.MATERIALUI,
+      TAGS.REDUX,
+      TAGS.GRAPHQL,
+      TAGS.JEST,
+    ],
   },
   {
-    title: "Earthquake and Volcano Visualizer - SGC",
+    title: "Viewers and Website SGC",
     description:
-      "Innovated a seismic and volcanic data viewer for SGC, improving UX and emergency response efficiency with enhanced geological data visualization.",
-    tecnologia: "React",
-    link: "",
-    github: "",
-    imagen: "/images/projects/sgc.webp",
-    tags: [TAGS.REACT, TAGS.SASS, TAGS.REDUX],
+      "The best viewer of earthquakes and volcanoes in Latin America",
+    link: "https://sgc.gov.co/sismos",
+    repo: "",
+    image: "/images/projects/sgc.webp",
+    tags: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.MATERIALUI, TAGS.SASS, TAGS.REDUX],
   },
   {
-    title: "E-Commerce Development - Lobocueros S.A.S",
+    title: "Lobocueros E-Commerce",
     description:
-      "Led e-commerce platform creation for Lobocueros, integrating a product catalog, payment process, and responsive design for optimal user engagement.",
-    tecnologia: "React",
+      "The virtual store of one of the longest-running leather goods stores in Colombia.",
     link: "",
     github: "",
-    imagen: "/images/projects/lobocueros.webp",
-    tags: [TAGS.REACT, TAGS.MATERIALUI, TAGS.SASS],
+    image: "/images/projects/lobocueros.webp",
+    tags: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.SASS, TAGS.REDUX],
   },
 ];
 
@@ -142,33 +159,33 @@ const EXPERIENCE = [
     date: "November, 2022 – February, 2024",
     title: "LinkedAI",
     description:
-      "Significantly enhanced platform usability focusing on user experience, accessibility, and customization, leading to increased user retention and reduced support costs. Optimized source code for scalability and maintenance, employing best development practices for clean, standardized code. Implemented AI for automated image tagging, reducing processing time. Developed a high-performance tool for interactive viewing and reviewing of large image datasets, improving data reliability and accuracy.",
-    link: "",
-    techStack: [TAGS.REACT, TAGS.MATERIALUI, TAGS.GRAPHQL, TAGS.REDUX],
+      "Actively involved in developing the fastest global platform for computer vision training data. I enhanced usability and performance, boosting user retention. I integrated advanced AI modules for auto-labeling and precise review of vast image datasets, ensuring unmatched data accuracy. I optimized the source code for better maintenance and scalability. I reduced complexity and implemented unit tests increasing system reliability. Additionally, i worked on the company's landing pages reinforced online presence and user accessibility, further solidifying market position.",
+    link: "https://www.linkedai.co/platform",
+    techStack: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.MATERIALUI, TAGS.GRAPHQL, TAGS.REDUX, TAGS.JEST, TAGS.TESTING_LIBRARY],
   },
   {
     date: "February, 2022 - September, 2022",
     title: "J&A Real Estate",
     description:
-      "Designed and developed an intuitive property cataloguing system from scratch, enhancing visualization, search, and filtering capabilities. This innovation significantly boosted organizational efficiency, employee productivity, and reduced task completion times, improving overall operational efficiency.",
+      "I created a unique property cataloging system, facilitating an intuitive and effective filtered search. This development has allowed the company's employees to improve their organization and productivity significantly. Reducing the time needed to complete internal tasks has improved operational efficiency. This advance represents a notable change in how the company manages and accesses property information, marking a before and after in its internal processes.",
     link: "",
-    techStack: [TAGS.REACT, TAGS.MATERIALUI, TAGS.GRAPHQL, TAGS.REDUX],
+    techStack: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.MATERIALUI, TAGS.REDUX, TAGS.JEST],
   },
   {
     date: "March, 2020 - November, 2021",
     title: "Colombian Geological Service (SGC)",
     description:
-      "Developed an advanced earthquake and volcano viewer from scratch, establishing it as the leading platform in Latin America. Enhanced design and code for better usability, accessibility, and performance, supporting real-time data for millions. Contributed to significant improvements in geological emergency response and public safety.",
-    link: "",
-    techStack: [TAGS.REACT, TAGS.MATERIALUI, TAGS.GRAPHQL, TAGS.REDUX],
+      "I developed the SGC earthquake and volcano viewer, the leader in Latin America, from scratch, capable of offering interactive seismological and volcanic data in real time. I made the layout and usability and adaptability improvements of the design. Optimized performance to support millions of data and users, creating an effective platform for geological emergencies. I contributed to a seismic activity search engine, increasing earthquake prevention in Colombia, and developed modules for the SGC website, expanding access to key services and information from the Ministry of Mines and Energy.",
+    link: "https://www.sgc.gov.co/",
+    techStack: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.MATERIALUI, TAGS.SASS, TAGS.GRAPHQL, TAGS.REDUX],
   },
   {
     date: "September, 2019 - February, 2020",
     title: "Lobocueros S.A.S",
     description:
-      "Led the design and development of an e-commerce platform from scratch, integrating comprehensive product catalogs, payment solutions, and a user-friendly responsive design. This project significantly enabled online sales and enhanced customer experience across devices.",
+      "I led the creation of an e-commerce for a long-standing Colombian leather goods store, offering an interactive catalog with filters and pagination, product details, and a shopping cart functionality with a secure payment gateway. This project, designed to adapt to any device, improves the online shopping experience, highlighting the tradition and quality of the brand.",
     link: "",
-    techStack: [TAGS.REACT, TAGS.MATERIALUI, TAGS.GRAPHQL, TAGS.REDUX],
+    techStack: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.MATERIALUI, TAGS.SASS, TAGS.GRAPHQL, TAGS.REDUX],
   },
 ];
 
