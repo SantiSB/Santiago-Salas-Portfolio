ebc44ee672e7be78afe56ede0ee445dc7926e209
SantiSB <a.santiago.salas.b@gmail.com>
2025-03-15 17:42:00 -0500

    laboral experience completed

--- src/components/experience/ExperienceItem.jsx
+++ src/components/experience/ExperienceItem.jsx
@@ -6,7 +6,7 @@ const cardVariants = {
   visible: { opacity: 1, scale: 1, transition: { duration: 0.7, ease: "easeInOut" } },
 };
 
-const ExperienceItem = ({ title, description, date, link, techStack, achievements = [] }) => {
+const ExperienceItem = ({ title, description, date, companyLink, techStack, projectsAndContributions = [] }) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: false });
 
@@ -22,10 +22,17 @@ const ExperienceItem = ({ title, description, date, link, techStack, achievement
       <h3 className="text-2xl font-semibold text-gray-800 dark:text-gray-200">{title}</h3>
       <p className="mt-2 text-base text-gray-600 dark:text-gray-400">{description}</p>
 
-      {achievements.length > 0 && (
+      {projectsAndContributions.length > 0 && (
         <ul className="list-disc list-inside mt-4 text-gray-600 dark:text-gray-400">
-          {achievements.map((achievement, i) => (
-            <li key={i}>{achievement}</li>
+          {projectsAndContributions.map((project, i) => (
+            <ul key={i}>
+              <strong>{project.name}:</strong>
+              <ul className="list-disc list-inside">
+                {project.details.map((detail, j) => (
+                  <li key={j}>{detail}</li>
+                ))}
+              </ul>
+            </ul>
           ))}
         </ul>
       )}
@@ -33,20 +40,17 @@ const ExperienceItem = ({ title, description, date, link, techStack, achievement
       <div className="flex flex-wrap gap-2 mt-4">
         {techStack.map((tech, i) => (
           <span key={i} className="flex items-center gap-2 px-3 py-1 bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-full text-xs">
-            <tech.icon className="w-4 h-4" />
             {tech.name}
           </span>
         ))}
       </div>
 
-      {link && (
-        <a href={link} className="inline-flex items-center mt-4 px-4 py-2 text-sm font-medium rounded-lg bg-gray-700 text-white hover:bg-gray-800 transition">
-          Visit
-          <svg className="w-4 h-4 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-          </svg>
-        </a>
-      )}
+      <a href={companyLink} target="_blank" className="inline-flex items-center mt-4 px-4 py-2 text-sm font-medium rounded-lg bg-gray-700 text-white hover:bg-gray-800 transition">
+        Visit Company
+        <svg className="w-4 h-4 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+      </a>
     </motion.div>
   );
 };
--- src/components/utils/Info.js
+++ src/components/utils/Info.js
@@ -1,107 +1,83 @@
-import ReactJS from "../../icons/stack/React.astro";
-import MaterialUI from "../../icons/stack/MaterialUI.astro";
-import Redux from "../../icons/stack/Redux.astro";
-import Sass from "../../icons/stack/Sass.astro";
-import AstroFramework from "../../icons/stack/AstroFramework.astro";
-import GitHub from "../../icons/GitHub.astro";
-import GitLab from "../../icons/stack/GitLab.astro";
-import JavaScript from "../../icons/stack/JavaScript.astro";
-import TypeScript from "../../icons/stack/TypeScript.astro";
-import Jest from "../../icons/stack/Jest.astro";
-import Vitest from "../../icons/stack/Vitest.astro";
-import CSS from "../../icons/stack/CSS.astro";
-import HTML from "../../icons/stack/HTML.astro";
-import TailwindCSS from "../../icons/stack/TailwindCSS.astro";
-import ThreeJS from "../../icons/stack/ThreeJS.astro";
-import GraphQL from "../../icons/stack/GraphQL.astro";
-import Netlify from "../../icons/stack/Netlify.astro";
-import Aws from "../../icons/stack/Aws.astro";
-
 const TAGS = {
   REACT: {
     name: "React.js",
-    icon: ReactJS,
   },
   MATERIALUI: {
     name: "Material UI",
-    icon: MaterialUI,
   },
   GRAPHQL: {
     name: "GraphQL",
-    icon: GraphQL,
   },
   REDUX: {
     name: "Redux",
-    icon: Redux,
   },
   SASS: {
     name: "Sass",
-    icon: Sass,
   },
   HTML: {
     name: "HTML",
-    icon: HTML,
   },
   CSS: {
     name: "CSS",
-    icon: CSS,
   },
   TAILWIND: {
     name: "Tailwind CSS",
-    icon: TailwindCSS,
   },
   THREE_JS: {
     name: "Three.js",
-    icon: ThreeJS,
   },
   ASTRO: {
     name: "Astro",
-    icon: AstroFramework,
   },
   AWS: {
     name: "AWS",
-    icon: Aws,
   },
   NETLIFY: {
     name: "NETLIFY",
-    icon: Netlify,
   },
   GITHUB: {
     name: "GitHub",
-    icon: GitHub,
   },
   GITLAB: {
     name: "GitLab",
-    icon: GitLab,
   },
   BITBUCKET: {
     name: "Bitbucket",
-    icon: "Bitbucket",
   },
   JAVASCRIPT: {
     name: "JavaScript",
-    icon: JavaScript,
   },
   TYPESCRIPT: {
     name: "TypeScript",
-    icon: TypeScript,
   },
   JEST: {
     name: "Jest",
-    icon: Jest,
   },
   TESTING_LIBRARY: {
     name: "Testing Library",
-    icon: "Testing-Library",
   },
   VITEST: {
     name: "Vitest",
-    icon: Vitest,
   },
   NEXT_JS: {
     name: "Next.js",
-    icon: "Next.js",
   },
+  FLUTTER: {
+    name: "Flutter",
+  },
+  FIGMA: {
+    name: "Figma", 
+  },
+  WORDPRESS: {
+    name: "WordPress",
+  },
+  FIREBASE: {
+    name: "Firebase",
+  },
+  REST_API: {
+    name: "Rest API",
+  },
+  
 };
 
 const PROJECTS = [
@@ -221,78 +197,156 @@ const PROJECTS = [
 
 const EXPERIENCE = [
   {
-    date: "May, 2024 – Present",
-    title: "57 Blocks",
+    title: "Frontend Developer at 57Blocks 🚀",
+    date: "May 2024 – Present",
+    companyLink: "https://www.57blocks.com/",
     description:
-      "57 Blocks is a software development firm specializing in building applications, artificial intelligence, and Web3. The company has launched more than 60 products since 2018, collaborating in industries like DeFi, FinTech, AdTech, AgTech, and HealthTech.",
-    achievements: [
-      "Developed an interactive and optimized website for Presidium Solutions, enhancing SEO and client engagement.",
-      "Implemented an ROI calculation application featuring interactive graphs, PDF generation, and automated email dispatch.",
-      "Optimized the Music City GP website to improve ticket sales and user experience.",
-      "Currently leading the development of an AI-based project management system to enhance productivity and planning.",
+      "57Blocks is a firm specializing in the development of digital products for startups and large companies. They have launched more than 60 technological solutions in different sectors, collaborating with companies looking to scale their high-impact products.\n\n" +
+      "🌍 Work with International Teams:\n" +
+      "- 🤝 Actively participated in meetings with clients and global teams.\n" +
+      "- 🗣️ Effectively communicated in English to discuss technical solutions, present ideas, and coordinate implementations.",
+    projectsAndContributions: [
+      {
+        name: "Presidium Solutions 💼",
+        details: [
+          "🎨 Designed and developed the website, optimizing customer acquisition with an attractive interface and SEO strategies.",
+          "📊 Implemented an ROI calculation application with graph generation and PDF export, as well as email integration to improve user experience and customer conversion.",
+        ],
+      },
+      {
+        name: "Music City GP (IndyCar Event) 🏎️",
+        details: [
+          "🌐 Contributed to the development of the event website, improving the user experience with key WordPress modules.",
+          "🎟️ Optimized the event's visibility and facilitated ticket purchases through improvements to the site's interface and performance.",
+        ],
+      },
+      {
+        name: "Presidium Solutions Mobile App 📱",
+        details: [
+          "⚙️ Implemented new features in the mobile app developed in Flutter, improving its stability and usability without affecting its operation.",
+          "💰 Contributed to the evolution of a revenue-generating tool, ensuring a better user experience.",
+        ],
+      },
+      {
+        name: "Intelligent Project Management System (AI) 🤖",
+        details: [
+          "🛠️ Developed an AI-based platform to optimize project planning and management.",
+          "📌 Improved requirements generation and organization, time estimates, and risk analysis.",
+          "🔗 Automated task structuring and integration with productivity tools, AI-powered automatic transcription, and online meeting platforms.",
+        ],
+      },
     ],
-    link: "https://www.57blocks.com/",
     techStack: [
-      // TAGS.REACT,
-      // // TAGS.NEXTJS,
-      // TAGS.TYPESCRIPT,
-      // TAGS.REDUX,
-      // TAGS.TAILWIND,
-      // TAGS.API_REST,
-      // TAGS.GRAPHQL,
-      // TAGS.JEST,
-      // TAGS.TESTING_LIBRARY,
-      // // TAGS.FIREBASE,
-      // // TAGS.WORDPRESS,
-      // // TAGS.FLUTTER,
-      // // TAGS.FIGMA,
+      TAGS.NEXT_JS,
+      TAGS.REACT,
+      TAGS.TYPESCRIPT,
+      TAGS.REDUX,
+      TAGS.TAILWIND,
+      TAGS.FIREBASE,
+      TAGS.WORDPRESS,
+      TAGS.FLUTTER,
+      TAGS.FIGMA,
+      TAGS.REST_API,
+      TAGS.GRAPHQL,
     ],
   },
   {
-    date: "November, 2022 – February, 2024",
-    title: "LinkedAI",
+    title: "Frontend Developer at LinkedAI 🤖",
+    date: "November 2022 – February 2024",
+    companyLink: "https://www.linkedai.co/",
     description:
-      "LinkedAI is a high-speed platform for AI training data creation, focusing on computer vision and natural language processing. The technology enables precise image annotation, AI-assisted review, and bias reduction.",
-    achievements: [
-      "Proposed and implemented improvements in system architecture and development workflows, enhancing code maintainability.",
-      "Developed advanced modules for image visualization with complex HTML elements, filters, search, and sorting options.",
-      "Integrated AI tools for automated image labeling and review, significantly reducing processing time.",
-      "Implemented unit and integration testing, improving stability and reducing errors in production.",
+      "LinkedAI is the fastest platform for creating and curating training datasets for artificial intelligence and machine learning models, with a focus on computer vision and natural language processing (NLP). Its technology enables advanced data annotation with high precision, optimizing AI model performance through automated labeling, AI-assisted review, and bias reduction.\n\n" +
+      "🌍 Interdisciplinary Collaboration:\n" +
+      "- 🔄 Worked closely with backend, design, and marketing teams to ensure an intuitive and accessible user interface aligned with business goals.",
+    projectsAndContributions: [
+      {
+        name: "Architecture and Development Flow Optimization 🏗️",
+        details: [
+          "📌 Proposed and implemented code organization improvements, standardizing styles and structures to enhance project coherence and maintainability.",
+        ],
+      },
+      {
+        name: "Performance and Scalability Enhancements ⚡",
+        details: [
+          "🔧 Refactored key functions, optimizing code complexity and reducing processing costs.",
+        ],
+      },
+      {
+        name: "Advanced Module Development 🖼️",
+        details: [
+          "🔍 Implemented tools for image visualization and segmentation with advanced filters and sorting options to streamline data review.",
+        ],
+      },
+      {
+        name: "AI Integration 🤖",
+        details: [
+          "🚀 Developed and integrated AI models for automatic image labeling and review, significantly reducing processing time and improving dataset quality.",
+        ],
+      },
+      {
+        name: "Testing and System Stability 🧪",
+        details: [
+          "✅ Implemented unit and integration tests using Jest and Testing Library, increasing system stability and reducing production errors.",
+        ],
+      },
+      {
+        name: "Conversion Optimization 📈",
+        details: [
+          "🎯 Contributed to improving and optimizing LinkedAI's sales page, maximizing customer conversion.",
+        ],
+      },
     ],
-    link: "https://www.linkedai.co/platform",
     techStack: [
-      // TAGS.REACT,
-      // TAGS.JAVASCRIPT,
-      // TAGS.REDUX,
-      // TAGS.MATERIALUI,
-      // // TAGS.API_REST,
-      // TAGS.GRAPHQL,
-      // TAGS.JEST,
-      // TAGS.TESTING_LIBRARY,
-      // TAGS.AWS,
+      TAGS.REACT,
+      TAGS.JAVASCRIPT,
+      TAGS.REDUX,
+      TAGS.MATERIALUI,
+      TAGS.REST_API,
+      TAGS.GRAPHQL,
+      TAGS.JEST,
+      TAGS.TESTING_LIBRARY,
+      TAGS.AWS,
     ],
   },
   {
-    date: "March, 2020 – November, 2021",
-    title: "Colombian Geological Service (SGC)",
+    title: "Frontend Developer at Servicio Geológico Colombiano 🌎",
+    date: "March 2020 – November 2021",
+    companyLink: "https://www.sgc.gov.co/",
     description:
-      "The Colombian Geological Service (SGC) is a government entity focused on geoscientific research, risk management, and natural resource monitoring.",
-    achievements: [
-      "Developed a real-time Earthquake and Volcano Viewer, incorporating multiple layers of geospatial data.",
-      "Integrated real-time seismic data with interactive maps, ensuring precise visualization and analysis.",
-      "Optimized system performance to handle large volumes of historical seismic data efficiently.",
-      "Led the migration of the styling structure, improving maintainability and scalability of the platform.",
-      "Developed a seismic activity cataloging system for government and research use, enhancing data accessibility.",
+      "The Servicio Geológico Colombiano (SGC), under the Ministry of Mines and Energy, leads geoscientific and nuclear research in Colombia, monitoring geological hazards and promoting risk management. As a frontend developer, I was responsible for designing and developing key digital tools for geological data visualization and analysis.",
+    projectsAndContributions: [
+      {
+        name: "Earthquake and Volcano Viewer 🌋",
+        details: [
+          "🌍 Developed the most advanced earthquake and volcano monitoring tool in Latin America, enabling real-time tracking of seismic and volcanic activity through interactive maps.",
+          "🗺️ Implemented dynamic maps with multiple information layers, integrating real-time data and detailed visualizations of each geological event.",
+          "⚡ Optimized the processing of large volumes of historical data, improving response speed and ensuring a seamless user experience.",
+          "🚀 Ensured system scalability and concurrency, preventing crashes and slowdowns under high-traffic queries.",
+          "⚙️ Enhanced website and visualization performance, optimizing load speed and graphical component integration without compromising system stability.",
+        ],
+      },
+      {
+        name: "Seismic Activity Catalog 📑",
+        details: [
+          "📊 Designed and developed a seismic activity catalog system for querying, editing, and filtering historical data, facilitating analysis for researchers and government entities.",
+          "🔍 Implemented an optimized interface for efficient searching and visualization of seismic and volcanic data.",
+        ],
+      },
+      {
+        name: "SGC Website 🌐",
+        details: [
+          "🖥️ Contributed to the optimization and development of multiple modules on the SGC website, ensuring a responsive and accessible experience.",
+          "🔄 Led the migration and restructuring of style code, improving project maintainability and scalability.",
+        ],
+      },
     ],
-    link: "https://www.sgc.gov.co/",
     techStack: [
-      // TAGS.REACT,
-      // TAGS.JAVASCRIPT,
-      // TAGS.REDUX,
-      // TAGS.MATERIALUI,
-      // TAGS.SASS,
-      // // TAGS.API_REST,
-      // TAGS.GITLAB,
+      TAGS.REACT,
+      TAGS.JAVASCRIPT,
+      TAGS.REDUX,
+      TAGS.SASS,
+      TAGS.MATERIALUI,
+      TAGS.REST_API,
     ],
   },
 ];
