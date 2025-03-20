136afa2bd058074873445e9b680c4e202574c034
SantiSB <a.santiago.salas.b@gmail.com>
2025-03-20 12:25:34 -0500

    fix: errors

--- src/icons/stack/Figma.jsx
+++ src/icons/stack/Figma.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const FigmaIcon = (props) => (
   <svg
     {...props}
--- src/icons/stack/Netlify.jsx
+++ src/icons/stack/Netlify.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const NetlifyIcon = (props) => (
   <svg
     {...props}
